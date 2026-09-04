4362b40edcc4ea237bf66b7784d7f247e152fda3
pashameed-art <pashameed@gmail.com>
2026-09-04 14:55:59 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
-const CACHE='upi-qr-v2';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE="upi-qr-v4";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"]))).then(()=>self.skipWaiting()));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
